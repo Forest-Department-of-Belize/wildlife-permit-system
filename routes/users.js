2004b55f472e4635fc1db8c9ce7a941134d0d85f
Jevon Teul <https://github.com/JevonTeul>
2026-04-29 13:36:15 -0600

    Fix users routes deactivate reactivate

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isLoggedIn, isAdminOrOIC } = require('../middleware/auth');
-const { isAdmin } = require('../middleware/auth');
+const { isLoggedIn, isAdminOrOIC, isAdmin } = require('../middleware/auth');
 const c = require('../controllers/userController');
 
 router.get('/profile', isLoggedIn, c.profile);
@@ -12,8 +11,8 @@ router.post('/', isLoggedIn, isAdmin, c.store);
 router.get('/:uuid', isLoggedIn, isAdminOrOIC, c.view);
 router.get('/:uuid/edit', isLoggedIn, isAdminOrOIC, c.edit);
 router.post('/:uuid/resend-invite', isLoggedIn, isAdmin, c.resendInvite);
-router.post('/:uuid', isLoggedIn, isAdminOrOIC, c.update);
 router.post('/:uuid/deactivate', isLoggedIn, isAdmin, c.deactivate);
 router.post('/:uuid/reactivate', isLoggedIn, isAdmin, c.reactivate);
+router.post('/:uuid', isLoggedIn, isAdminOrOIC, c.update);
 
 module.exports = router;
