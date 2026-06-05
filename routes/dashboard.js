f3f2e02ace4c0ce4099a53fe2a963cf7120d7cc2
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2026-06-05 17:00:31 -0600

    feat: finish migration of new code

--- routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isLoggedIn } = require('../middleware/auth');
-const dashboardController = require('../controllers/dashboardController');
-
-router.get('/', isLoggedIn, dashboardController.index);
-router.get('/charts', isLoggedIn, dashboardController.chartData);
-
-module.exports = router;
--- routes/import.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isLoggedIn, isAdmin } = require('../middleware/auth');
-const importController = require('../controllers/importController');
-
-router.get('/', isLoggedIn, isAdmin, importController.getImport);
-router.post('/', isLoggedIn, isAdmin, importController.upload.single('file'), importController.postImport);
-
-module.exports = router;
